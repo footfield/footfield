d2d11202f7b6cbb5d31c8b0e51b1f0ecaa9aa576
khoa <bkhoa0401@gmail.com>
2018-11-10 12:33:34 -0800

    update

--- Project_SWD_ManagerFootField/app/src/main/java/khoa/example/com/project_swd_managerfootfield/DetailPitchActivity.java
+++ Project_SWD_ManagerFootField/app/src/main/java/khoa/example/com/project_swd_managerfootfield/DetailPitchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -12,7 +13,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import khoa.example.com.project_swd_managerfootfield.VM.LocationInfoVM;
 
@@ -22,6 +25,14 @@ public class DetailPitchActivity extends AppCompatActivity {
     Spinner spTypePitch, spPitchName;
     CheckBox cbSlot;
     LinearLayout linearOfCbSlot;
+
+    List<TypePitch> listTypePitch;
+    List<String> descriptionOfTypePitch;
+    Map<Integer, String> mapTypePitch;
+
+    List<PitchDetail> listPitchByTypeAndSlot;
+    List<String> listNameOfPitchByTypeAndSlot;
+    Map<Integer, String> mapPitchByTypeAndSlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +45,64 @@ public class DetailPitchActivity extends AppCompatActivity {
 
         //set spiner list type pitch
         spTypePitch = findViewById(R.id.spTypePitch);
-        List<String> listType = new ArrayList<>();
-        listType.add("sân 5 ");
-        listType.add("sân 7 ");
-        listType.add("sân 11");
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listType);
+
+        // tra 1 list type pitch
+        listTypePitch = new ArrayList<>();
+        descriptionOfTypePitch = new ArrayList<>();
+        mapTypePitch = new HashMap<>();
+        for (int i = 0; i < listTypePitch.size(); i++) {
+            descriptionOfTypePitch.add(listTypePitch.get(i).getDescription());
+            mapTypePitch.put(listTypePitch.get(i).getId(), listTypePitch.get(i).getDescription());
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, descriptionOfTypePitch);
         spTypePitch.setAdapter(adapter);
+        spTypePitch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //return key of pitch
+                int key = returnKeyOfPitchType(mapTypePitch, spTypePitch);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
         spPitchName = findViewById(R.id.spPitchName);
-        List<String> listPitchByTypeAndSlot = new ArrayList<>();
-        listPitchByTypeAndSlot.add("sân 1");
-        listPitchByTypeAndSlot.add("sân 2");
-        listPitchByTypeAndSlot.add("sân 3");
-        listPitchByTypeAndSlot.add("sân 4");
-        listPitchByTypeAndSlot.add("sân 5");
+        //return 1 list pitch when filter type pitch and slot
+        listPitchByTypeAndSlot = new ArrayList<>();
+        listNameOfPitchByTypeAndSlot = new ArrayList<>();
+        mapPitchByTypeAndSlot = new HashMap<>();
+
+        for (int i = 0; i < listPitchByTypeAndSlot.size(); i++) {
+            listNameOfPitchByTypeAndSlot.add(listPitchByTypeAndSlot.get(i).getDescription());
+            mapPitchByTypeAndSlot.put(listPitchByTypeAndSlot.get(i).getId(), listPitchByTypeAndSlot.get(i).getDescription());
+        }
+
         ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listPitchByTypeAndSlot);
         spPitchName.setAdapter(adapter2);
 
-        linearOfCbSlot=findViewById(R.id.linearOfCbSlot);
-        for (int i=0;i<3;i++){
-            CheckBox cbSlot=new CheckBox(this);
-            cbSlot.setText("slot "+(i+1));
+        spPitchName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //return key of pitch when filter type pitch and slot
+                int key = returnKeyOfPitchType(mapPitchByTypeAndSlot, spPitchName);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        linearOfCbSlot = findViewById(R.id.linearOfCbSlot);
+        for (int i = 0; i < 3; i++) {
+            CheckBox cbSlot = new CheckBox(this);
+            cbSlot.setText("slot " + (i + 1));
             linearOfCbSlot.addView(cbSlot);
         }
 
@@ -74,5 +121,15 @@ public class DetailPitchActivity extends AppCompatActivity {
     }
 
     public void clickToBookField(View view) {
+    }
+
+    public int returnKeyOfPitchType(Map<Integer, String> map, Spinner spinner) {
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            String description = spinner.getSelectedItem().toString();
+            if (entry.getValue().equals(description)) {
+                return entry.getKey();
+            }
+        }
+        return 0;
     }
 }
