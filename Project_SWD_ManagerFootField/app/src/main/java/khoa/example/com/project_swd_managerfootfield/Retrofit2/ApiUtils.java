86ab71c93c2aaa3c85ea9b1dd4f21585ee49419c
donnse62522 <donnse62522@users.noreply.github.com>
2018-11-14 00:23:54 +0700

    update

--- Project_SWD_ManagerFootField/app/src/main/java/khoa/example/com/project_swd_managerfootfield/Retrofit2/ApiUtils.java
+++ Project_SWD_ManagerFootField/app/src/main/java/khoa/example/com/project_swd_managerfootfield/Retrofit2/ApiUtils.java
@@ -1,10 +1,10 @@
 package khoa.example.com.project_swd_managerfootfield.Retrofit2;
 
 public class ApiUtils {
-    public static final String BASE_URL = "http://25.8.22.194:1234/api/";
+//    public static final String BASE_URL = "http://25.8.22.194:1234/api/";
 //    public static final String BASE_URL = "http://10.82.138.41:1234/api/";
 //    public static final String BASE_URL = "http://192.168.43.196:1234/api/";
-    // public static final String BASE_URL = "http://192.168.1.10:1234/api/";
+     public static final String BASE_URL = "http://192.168.1.10:1234/api/";
     //public static final String BASE_URL = "http://192.168.137.1:1234/api/";
    // public static final String BASE_URL = "http://172.16.3.155:1234/api/";
 
